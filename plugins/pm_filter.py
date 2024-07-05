68d981b6e7f5d623eb74e304e38791de9c3aaad8
mikhael324 <83812749+mikhael324@users.noreply.github.com>
2024-07-05 17:33:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -277,7 +277,7 @@ async def advantage_spoll_choker(bot, query):
                  text=f"<b>#NoResult \n\nMovie | Series : <code>{movie}</code> \n \n@Manu3324</b>"
             )
             k = await query.message.edit(f"\n👋 Hello {reqstr.mention},\n\n <b> Your Movie: </b><b>'{movie}'</b><b> \n \n Will Be Uploaded Here 👇 Join Now </b>",
-                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⭕ Join Here ⭕", url="https://t.me/+oS24K7-AvBE3ODk9")]]))
+                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⭕ Join Here ⭕", url="https://t.me/+pqwYZUmHE3w2YmU1")]]))
             await asyncio.sleep(30)
             await k.delete()
             await query.message.reply_to_message.delete()
